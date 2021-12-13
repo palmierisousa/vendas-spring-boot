4bc54ea5700bd773bb7bae084b60c55e1b767a10
palmierisousa <palmierisousa@gmail.com>
2021-12-13 17:47:31 -0300

    Criação de profiles de desenvolvimento e produção.

--- src/main/java/io/github/palmierisousa/SalesApplication.java
+++ src/main/java/io/github/palmierisousa/SalesApplication.java
@@ -2,9 +2,10 @@ package io.github.palmierisousa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalesApplication {
+public class SalesApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
     }
